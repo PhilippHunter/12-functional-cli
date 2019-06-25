d8c214ff14e33a77b77587fca69b1f5643437000
PhilippHunter <philippjaeger98@web.de>
2019-06-25 12:25:51 +0200

    tests

--- src/test/java/ohm/softa/a12/icndb/JokesGeneratorTests.java
+++ src/test/java/ohm/softa/a12/icndb/JokesGeneratorTests.java
@@ -1,6 +1,16 @@
 package ohm.softa.a12.icndb;
 
+import ohm.softa.a12.model.JokeDto;
+import ohm.softa.a12.model.ResponseWrapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 /**
  * @author Peter Kurfer
@@ -8,18 +18,36 @@ import org.junit.jupiter.api.Test;
  */
 class JokesGeneratorTests {
 
-    private JokeGenerator jokeGenerator = new JokeGenerator();
+	private static final Logger logger = LogManager.getLogger();
+	private JokeGenerator jokeGenerator = new JokeGenerator();
 
-    @Test
-    void testRandomStream() {
-        /* timeout to ensure that stream does not loop forever */
-        /* TODO implement a test for the random joke stream */
-    }
+	@Test
+	void testRandomStream() {
+		/* timeout to ensure that stream does not loop forever */
+		/* TODO implement a test for the random joke stream */
+		assertTimeout(Duration.ofSeconds(5L), () -> jokeGenerator.randomJokesStream()
+			.filter(Objects::nonNull)
+			.skip(3)
+			.limit(5)
+			.map(ResponseWrapper::getValue)
+			.map(JokeDto::getJoke)
+			.forEach(Assertions::assertNotNull)
+		);
+	}
 
 
-    @Test
-    void testJokesStream() {
-        /* TODO implement a test for the linear jokes generator */
-    }
+	@Test
+	void testJokesStream() {
+		/* TODO implement a test for the linear jokes generator */
+		assertTimeout(Duration.ofMinutes(1L), () -> jokeGenerator.jokesStream()
+			.filter(Objects::nonNull)
+			.map(ResponseWrapper::getValue)
+			.skip(200L)
+			.limit(400L)
+			.map(JokeDto::getJoke)
+			.forEach(Assertions::assertNotNull)
+		);
+
+	}
 
 }
